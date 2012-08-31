b29a085ea20e4adfb366b6b44cc35d21a6d94a81
Dekmabot <dekmabot@gmail.com>
2012-08-31 09:18:27 +0600

    texts

--- core_2.14/core.php
+++ core_2.14/core.php
@@ -1,6 +1,6 @@
 <?php
 
-//До момента запуска ядра ошибки показываются, потом - в зависимости от режима test_mode
+// До момента запуска ядра ошибки показываются, потом - в зависимости от режима test_mode
 error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE ^ E_STRICT);
 ini_set("display_errors", "on");
 
@@ -9,20 +9,21 @@ session_start();
 ini_set('error_log', $config['path']['www'].'../error_'.$_SERVER['HTTP_HOST'].'.log');
 ini_set('include_path', implode(';',$config['path']));
 
-//Система контроля исключений работы системы
+// Система контроля исключений работы системы
 require_once($config['path']['core'].'/classes/log.php');
 $log=new log($config);
 
-//Кеш
+// Кеш
 require_once($config['path']['core'].'/classes/cache.php');
 $cache=new cache($config,$log);
 $cache->readCache();
 
-//Объявленная переменная позволяет просто запустить ядро, без выполнения контроллеров
+// Объявленная переменная позволяет просто запустить ядро, без выполнения контроллеров
 if( !$start_only ){
-	//Менеджер контроллеров
+	// Менеджер контроллеров
 	require_once($config['path']['core'].'/classes/controller_manager.php');
 	$controller_manager=new controller_manager($config,$log,$cache);
+	
 	exit();
 
 }else{
