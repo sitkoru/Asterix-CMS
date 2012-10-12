b4b4167cdf8bdc10190359989c489f8005a84b05
Dekmabot <dekmabot@gmail.com>
2012-10-12 17:11:32 +0600

    auth

--- core_2.14/classes/user.php
+++ core_2.14/classes/user.php
@@ -288,6 +288,9 @@ class user
 				echo("<script> top.location.href='" . $dialog_url . "'</script>");
 			}
 
+pr_r( $_REQUEST );
+pr_r( $_SESSION );
+
 			//Получаем Token
 			$token_url = 'https://oauth.vkontakte.ru/access_token?client_id='.$app_id.'&client_secret='.$app_secret.'&code='.$code;
 			$token = (array)json_decode(@file_get_contents($token_url));
