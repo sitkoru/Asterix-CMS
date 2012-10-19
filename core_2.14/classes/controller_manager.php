74a7908e42121de68b2b2a7f32577297eaf3206a
Dekmabot <dekmabot@gmail.com>
2012-10-19 10:57:22 +0600

    txt output format

--- core_2.14/classes/controller_manager.php
+++ core_2.14/classes/controller_manager.php
@@ -22,7 +22,7 @@ class controller_manager
 	public static $config;
 	public $vars = array();
 	
-	public static $output_formats = array('html','json','xml','tpl');
+	public static $output_formats = array('html','json','xml','tpl','txt');
 	
 	//Все стандартные контроллеры
 	public $controllers = array(
