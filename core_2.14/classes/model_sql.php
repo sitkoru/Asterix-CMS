675e0014bda38c64aa392ddabbb397498183a9b4
SonicGD <sonicgd@gmail.com>
2013-04-09 00:08:42 +0600

    Enable sql debug. Linebreaks.

--- core_2.14/classes/model_sql.php
+++ core_2.14/classes/model_sql.php
@@ -71,7 +71,7 @@ class ModelSql
         $sql_stop = $t[1] + $t[0];
         $time = $sql_stop - $sql_start;
         if (defined("SQL_DEBUG_LOG") && defined("SQL_DEBUG_LOG_HANDLE")) {
-            fwrite(SQL_DEBUG_LOG_HANDLE, $sql . " - " . $time);
+            fwrite(SQL_DEBUG_LOG_HANDLE, $sql . " - " . $time . "\n");
         }
         //Статистика
         if (!$cache) log::sql($sql, $time, $result_count, $query_type, $database);
