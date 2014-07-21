4545df23e87cc8e414e0bf0fbef6ed71df086546
brainsrage@gmail.com <2548561qwe>
2014-07-21 12:40:09 +0600

    -fix ckeditor

--- src/3.0/ckeditor/config.js
+++ src/3.0/ckeditor/config.js
@@ -10,21 +10,16 @@ CKEDITOR.editorConfig = function (config) {
 
     // The toolbar groups arrangement, optimized for two toolbar rows.
     config.toolbar = [
-        { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
-        { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
-        { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
-        '/',
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-        { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-        { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
-        '/',
-        { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
-        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-        { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
-        { name: 'others', items: [ '-' ] },
-        { name: 'about', items: [ 'About' ] }
+        'Source', '-', 'FontSize', 'Format', '-', 'Bold', 'Italic', 'Underline', '-',
+        'Subscript', 'Superscript', 'SpecialChar', '-',
+        'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyFull', '-',
+        'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-',
+        'Link', 'Unlink', 'Anchor', '-',
+        'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField', '-',
+        'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', 'BidiLtr', 'BidiRtl', '-',
+        'TextColor', 'BGColor', '-',
+        'UIColor', 'Maximize', 'ShowBlocks', '-',
+        'Table', 'Flash', 'Image', '-', 'PasteFromWord', 'RemoveFormat', 'Blockquote', 'typograf'
     ];
     config.toolbarGroups = [
         {name: 'clipboard', groups: [ 'clipboard', 'undo', 'paste', 'pastefromword', 'copy' ]},
