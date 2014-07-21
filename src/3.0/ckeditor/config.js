7fbe7960eb124d727fe64babf558c805fcf93d64
brainsrage@gmail.com <2548561qwe>
2014-07-21 14:22:07 +0600

    -fix ckeditor

--- src/3.0/ckeditor/config.js
+++ src/3.0/ckeditor/config.js
@@ -3,53 +3,8 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function (config) {
-    // Define changes to default configuration here.
-    // For complete reference see:
-    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-    // The toolbar groups arrangement, optimized for two toolbar rows.
-    config.toolbar = [
-        ['Source', '-', 'FontSize', 'Format', '-', 'Bold', 'Italic', 'Underline', '-',
-        'Subscript', 'Superscript', 'SpecialChar', '-',
-        'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyFull', '-',
-        'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-',
-        'Link', 'Unlink', 'Anchor', '-',
-        'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField', '-',
-        'NumberedList', 'BulletedList', 'Outdent', 'Indent', 'Blockquote', 'CreateDiv', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', 'BidiLtr', 'BidiRtl', '-',
-        'TextColor', 'BGColor', '-',
-        'UIColor', 'Maximize', 'ShowBlocks', '-',
-        'Table', 'Flash', 'Image', '-', 'PasteFromWord', 'RemoveFormat', 'Blockquote', 'typograf']
-    ];
-    config.toolbarGroups = [
-        {name: 'clipboard', groups: [ 'clipboard', 'undo', 'paste', 'pastefromword', 'copy' ]},
-        {name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ]},
-        {name: 'links'},
-        {name: 'insert'},
-        {name: 'forms'},
-        {name: 'tools'},
-        {name: 'document', groups: [ 'mode', 'document', 'doctools' ]},
-        {name: 'others'},
-        '/',
-        {name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ]},
-        {name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ]},
-        {name: 'styles'},
-        {name: 'colors'},
-        {name: 'about'}
-    ];
-
-    config.toolbar_Basic = [
-        [ 'Source', '-', 'Bold', 'Italic' ]
-    ];
-// Load toolbar_Name where Name = Basic.
-    config.toolbar = 'Basic';
-    // Remove some buttons provided by the standard plugins, which are
-    // not needed in the Standard(s) toolbar.
-    config.removeButtons = 'Underline,Subscript,Superscript';
-
-    // Set the most common block elements.
-    config.format_tags = 'p;h1;h2;h3;pre';
-
-    // Simplify the dialog windows.
-    config.removeDialogTabs = 'image:advanced;link:advanced';
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 };
