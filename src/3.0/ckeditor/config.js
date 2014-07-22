14b400d99670e0e23ca4d2475d58a3df9d719a99
brainsrage@gmail.com <2548561qwe>
2014-07-22 10:03:04 +0600

    -fix ckeditor

--- src/3.0/ckeditor/config.js
+++ src/3.0/ckeditor/config.js
@@ -5,6 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
     config.forcePasteAsPlainText = true;
+    config.allowedContent = true;
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
