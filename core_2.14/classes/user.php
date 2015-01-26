7a09365f615fd64dff6686e9140f47ff06f64a3c
brainsrage@gmail.com <2548561qwe>
2015-01-26 11:47:03 +0500

    test

--- core_2.14/classes/user.php
+++ core_2.14/classes/user.php
@@ -26,7 +26,6 @@ class user
 	public $auth_types = array('user' => 'Регистрация на сайте', 'yandex' => 'Яндекс', 'google' => 'Google', 'livejournal' => 'LiveJournal', 'openid' => 'OpenId');
 
 	public function __construct(){
-		die();
 		//Logout
 		if (IsSet($_GET['logout'])) {
 			self::logout();
@@ -189,7 +188,7 @@ class user
 				user::deleteCookie('auth');
 				$result = array(
 					'result' => 'message',
-					'message' => 'Не получилось, попробуйте ещё!',
+					'message' => 'Не получилось, попробуйте ещё',
 					'close' => false,
 				);
 			}
